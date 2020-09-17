88b37e535ede1c8afb5e44feee14ff317358a450
Joshua <joshua.oguma@outlook.com>
2020-09-17 19:50:42 +0300

    added web server

---
+++ build/precache-manifest.fe2c39a9e1cd35100154c2d9fe637afc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c96c20ad8975974829acd4739457681e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "642498ad03f0f4b25120",
+    "url": "/static/css/main.167fc0d5.chunk.css"
+  },
+  {
+    "revision": "88f4f4cdda0024fcb86e",
+    "url": "/static/js/2.db70fa4b.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.db70fa4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "642498ad03f0f4b25120",
+    "url": "/static/js/main.7767ed75.chunk.js"
+  },
+  {
+    "revision": "25d48e391914dc08b45e",
+    "url": "/static/js/runtime-main.d89612ff.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('./build'));
+
+app.listen(PORT, () => {
+  console.log(`App Started at Port ${PORT}`);
+});
